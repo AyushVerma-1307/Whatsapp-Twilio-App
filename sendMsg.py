788c00c27ad27e2f15ca8fac2a45501123d0962f
Ayush Verma <104214949+AyushVerma-1307@users.noreply.github.com>
2023-02-11 14:54:52 +0530

    Update sendMsg.py

--- sendMsg.py
+++ sendMsg.py
@@ -2,7 +2,7 @@ from twilio.rest import Client
 
 def message(number,message):
     sid='AC23f06e1e176da7ab10fe40b368e4b914'
-    authToken='a3dd07a3195cca1d383125605d31b250'
+    authToken='a3dd07a3195cca1d383125605d31b111'
     client=Client(sid,authToken)
     
     message=client.messages.create(to='whatsapp:'+number,
